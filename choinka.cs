6c6f436a4917a90c0f2e96d2254198400b175b2f
sin79 <38588080+sin79@users.noreply.github.com>
2018-04-21 17:45:37 +0200

    Add files via upload

---
+++ choinka.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int a, i, j, x, y, z;
+            string gwiazdka, puste;
+            Console.WriteLine("Podaj wysokosc choinki: ");
+            n = int.Parse(Console.ReadLine());
+            gwiazdka = "*";
+            puste = " ";
+            a = x;
+            for (i = 1; i <= x; i++)
+            {
+                for (j = 1; j < a; j++)
+                {
+                    Console.Write(nic);
+                }
+                for (y = 1; y <= i; y++)
+                {
+                    Console.Write(gwiazdka);
+                }
+                for (z = 1; z <= i - 1; z++)
+                {
+                    Console.Write(gwiazdka);
+                }
+                a--;
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
